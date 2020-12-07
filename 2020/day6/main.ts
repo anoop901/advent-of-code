2cb8662e6d166bc414b2ebf47dbc7233191f2a0b
anoop901 <anoop901@gmail.com>
2020-12-07 03:24:04 +0000

    day 6 part 2

--- 2020/day6/main.ts
+++ 2020/day6/main.ts
@@ -46,20 +46,48 @@ function union<T>(sets: Set<T>[]): Set<T> {
   return result;
 }
 
+function intersection<T>(sets: Set<T>[]): Set<T> {
+  if (sets.length === 0) {
+    return new Set();
+  }
+  const [firstSet, ...restOfSets] = sets;
+
+  const result = new Set(firstSet);
+  for (const otherSet of restOfSets) {
+    for (const value of result) {
+      if (!otherSet.has(value)) {
+        result.delete(value);
+      }
+    }
+  }
+
+  return result;
+}
+
 function countQuestionsWhereAnyAnsweredYes(customsForms: CustomsForm[]): number {
   return union(customsForms).size;
 }
 
-function getAnswer(customsFormsData: CustomsForm[][]): number {
+function getAnswerPart1(customsFormsData: CustomsForm[][]): number {
   return customsFormsData
     .map(countQuestionsWhereAnyAnsweredYes)
     .reduce((a, b) => a + b, 0);
 }
 
+function countQuestionsWhereAllAnsweredYes(customsForms: CustomsForm[]): number {
+  return intersection(customsForms).size;
+}
+
+function getAnswerPart2(customsFormsData: CustomsForm[][]): number {
+  return customsFormsData
+    .map(countQuestionsWhereAllAnsweredYes)
+    .reduce((a, b) => a + b, 0);
+}
+
 async function main() {
   const customsFormsData = await loadCustomsFormsData();
-  const answer = getAnswer(customsFormsData);
-  console.log(answer);
+  console.log(getAnswerPart1(customsFormsData));
+  console.log(getAnswerPart2(customsFormsData));
 }
 
 main();
