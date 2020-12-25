d441b3aa584d119b248fcbd665f3f56c1533ad46
Anoop Naravaram <anoop901@gmail.com>
2020-12-24 20:14:30 -0600

    create pipelines utility

---
+++ 2020/util/pipelines.ts
@@ -0,0 +1,19 @@
+import { Pipe } from "stream";
+
+class Pipeline<T, R> {
+  constructor(private readonly fn: (arg: T) => R) {}
+  then<S>(nextFn: (arg: R) => S): Pipeline<T, S> {
+    return new Pipeline((arg) => nextFn(this.fn(arg)));
+  }
+  run(value: T): R {
+    return this.fn(value);
+  }
+}
+
+export function buildPipeline<T, R>(fn: (arg: T) => R): Pipeline<T, R> {
+  return new Pipeline(fn);
+}
+
+export function runPipeline<T, R>(value: T, pipeline: Pipeline<T, R>): R {
+  return pipeline.run(value);
+}
