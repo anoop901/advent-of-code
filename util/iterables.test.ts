29dc344ed409e6da8ab3db134c5e7a141941a253
Anoop Naravaram <anoop901@gmail.com>
2020-12-27 20:54:21 -0600

    create repeat utility

--- util/iterables.test.ts
+++ util/iterables.test.ts
@@ -20,6 +20,7 @@ import {
   minBy,
   pairs,
   reduce,
+  repeat,
   slice,
   split,
   sum,
@@ -683,6 +684,26 @@ describe("iterables", () => {
           .then(anyMatch((x) => x < 10))
           .end()
       ).to.be.false;
+    });
+  });
+  describe("repeat", () => {
+    it("basic", () => {
+      expect(chain(repeat("buffalo", 3)).then(toArray).end()).to.deep.equal([
+        "buffalo",
+        "buffalo",
+        "buffalo",
+      ]);
+    });
+    it("0 times", () => {
+      expect(chain(repeat("buffalo", 0)).then(toArray).end()).to.deep.equal([]);
+    });
+    it("infinite", () => {
+      const iter = repeat("buffalo")[Symbol.iterator]();
+      expect(iter.next()).to.deep.equal({ value: "buffalo", done: false });
+      expect(iter.next()).to.deep.equal({ value: "buffalo", done: false });
+      expect(iter.next()).to.deep.equal({ value: "buffalo", done: false });
+      expect(iter.next()).to.deep.equal({ value: "buffalo", done: false });
+      expect(iter.next()).to.deep.equal({ value: "buffalo", done: false });
     });
   });
 });
--- util/iterables.ts
+++ util/iterables.ts
@@ -243,3 +243,18 @@ export function anyMatch<T>(pred: (t: T) => boolean) {
       .end();
   };
 }
+
+export function* repeat<T>(
+  value: T,
+  count: number | null = null
+): Generator<T> {
+  if (count != null) {
+    for (let i = 0; i < count; i++) {
+      yield value;
+    }
+  } else {
+    while (true) {
+      yield value;
+    }
+  }
+}
