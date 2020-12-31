f6535a30256a3b6bc51bb4d97acd7b827a28fa54
Anoop Naravaram <anoop901@gmail.com>
2020-12-31 11:45:48 -0600

    iterators splitAt

--- util/iterables.test.ts
+++ util/iterables.test.ts
@@ -23,6 +23,7 @@ import {
   repeat,
   slice,
   split,
+  splitAt,
   sum,
   take,
   takeWhile,
@@ -704,6 +705,56 @@ describe("iterables", () => {
       expect(iter.next()).to.deep.equal({ value: "buffalo", done: false });
       expect(iter.next()).to.deep.equal({ value: "buffalo", done: false });
       expect(iter.next()).to.deep.equal({ value: "buffalo", done: false });
+    });
+  });
+  describe("splitAt", () => {
+    it("complete before, then complete after", () => {
+      const { before, after } = splitAt(3)(
+        ["the", "quick", "brown", "fox", "jumps"][Symbol.iterator]()
+      );
+      expect(before.next()).to.deep.equal({ value: "the", done: false });
+      expect(before.next()).to.deep.equal({ value: "quick", done: false });
+      expect(before.next()).to.deep.equal({ value: "brown", done: false });
+      expect(before.next()).to.deep.equal({ value: undefined, done: true });
+      expect(after.next()).to.deep.equal({ value: "fox", done: false });
+      expect(after.next()).to.deep.equal({ value: "jumps", done: false });
+      expect(after.next()).to.deep.equal({ value: undefined, done: true });
+    });
+    it("complete after, then complete before", () => {
+      const { before, after } = splitAt(3)(
+        ["the", "quick", "brown", "fox", "jumps"][Symbol.iterator]()
+      );
+      expect(after.next()).to.deep.equal({ value: "fox", done: false });
+      expect(after.next()).to.deep.equal({ value: "jumps", done: false });
+      expect(after.next()).to.deep.equal({ value: undefined, done: true });
+      expect(before.next()).to.deep.equal({ value: "the", done: false });
+      expect(before.next()).to.deep.equal({ value: "quick", done: false });
+      expect(before.next()).to.deep.equal({ value: "brown", done: false });
+      expect(before.next()).to.deep.equal({ value: undefined, done: true });
+    });
+    it("interleave iterators, before first", () => {
+      const { before, after } = splitAt(3)(
+        ["the", "quick", "brown", "fox", "jumps"][Symbol.iterator]()
+      );
+      expect(before.next()).to.deep.equal({ value: "the", done: false });
+      expect(after.next()).to.deep.equal({ value: "fox", done: false });
+      expect(before.next()).to.deep.equal({ value: "quick", done: false });
+      expect(before.next()).to.deep.equal({ value: "brown", done: false });
+      expect(before.next()).to.deep.equal({ value: undefined, done: true });
+      expect(after.next()).to.deep.equal({ value: "jumps", done: false });
+      expect(after.next()).to.deep.equal({ value: undefined, done: true });
+    });
+    it("interleave iterators, after first", () => {
+      const { before, after } = splitAt(3)(
+        ["the", "quick", "brown", "fox", "jumps"][Symbol.iterator]()
+      );
+      expect(after.next()).to.deep.equal({ value: "fox", done: false });
+      expect(before.next()).to.deep.equal({ value: "the", done: false });
+      expect(before.next()).to.deep.equal({ value: "quick", done: false });
+      expect(after.next()).to.deep.equal({ value: "jumps", done: false });
+      expect(after.next()).to.deep.equal({ value: undefined, done: true });
+      expect(before.next()).to.deep.equal({ value: "brown", done: false });
+      expect(before.next()).to.deep.equal({ value: undefined, done: true });
     });
   });
 });
--- util/iterables.ts
+++ util/iterables.ts
@@ -258,3 +258,51 @@ export function* repeat<T>(
     }
   }
 }
+
+export function splitAt<T>(splitIndex: number) {
+  return (
+    iterator: Iterator<T>
+  ): { before: Iterator<T>; after: Iterator<T> } => {
+    let beforeBuffer: T[] | null = null;
+    let currentIndexBefore = 0;
+
+    const before: Iterator<T> = {
+      next(): IteratorResult<T> {
+        if (currentIndexBefore < splitIndex) {
+          // TODO get from buffer if present
+          currentIndexBefore++;
+          if (beforeBuffer !== null) {
+            const value = beforeBuffer.shift();
+            if (value === undefined) {
+              throw new Error("unreachable");
+            }
+            return { value, done: false };
+          } else {
+            return iterator.next();
+          }
+        } else {
+          return { done: true, value: undefined };
+        }
+      },
+    };
+    const after: Iterator<T> = {
+      next(): IteratorResult<T> {
+        if (currentIndexBefore < splitIndex && beforeBuffer === null) {
+          beforeBuffer = [];
+          for (let index = currentIndexBefore; index < splitIndex; index++) {
+            const { value, done } = iterator.next();
+            if (!done) {
+              beforeBuffer.push(value);
+            } else {
+              throw new Error("the iterable was shorter than the split index");
+
+              break;
+            }
+          }
+        }
+        return iterator.next();
+      },
+    };
+    return { before, after };
+  };
+}
