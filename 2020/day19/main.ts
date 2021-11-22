d33b0f13bce0feb6f90ab722324e8cb5f46d6001
Anoop Naravaram <anoop901@gmail.com>
2021-11-22 14:42:42 -0800

    2020 day 19 partial

--- 2020/day19/main.ts
+++ 2020/day19/main.ts
@@ -1,11 +1,13 @@
 import { chain } from "@anoop901/js-util";
+import map from "@anoop901/js-util/iterables/map";
 import split from "@anoop901/js-util/iterables/split";
-import splitAt from "@anoop901/js-util/iterables/splitAt";
 import toArray from "@anoop901/js-util/iterables/toArray";
 import loadInputLines from "../../util/loadInputLines";
+import peg from "pegjs";
 import { Rule, RuleSet } from "./Rule";
+import countMatching from "@anoop901/js-util/iterables/countMatching";
 
-async function parseRules(): Promise<{ ruleSet: RuleSet; messages: string[] }> {
+async function parseInput(): Promise<{ ruleSet: RuleSet; messages: string[] }> {
   const ruleSet = new Map<number, Rule>();
 
   const lines = await loadInputLines();
@@ -31,16 +33,79 @@ async function parseRules(): Promise<{ ruleSet: RuleSet; messages: string[] }> {
     const possibleSequences = ruleSequencesStrings.map((ruleSequenceString) =>
       ruleSequenceString.split(" ").map(Number)
     );
-    console.log(possibleSequences);
     ruleSet.set(ruleNumber, { ruleType: "CompositeRule", possibleSequences });
   }
 
   return { ruleSet, messages };
 }
 
-(async () => {
-  console.log(await parseRules());
+function updateRuleSet(ruleSet: RuleSet): void {
+  ruleSet.set(8, {
+    ruleType: "CompositeRule",
+    possibleSequences: [[42], [42, 8]],
+  });
+  ruleSet.set(11, {
+    ruleType: "CompositeRule",
+    possibleSequences: [
+      [42, 31],
+      [42, 11, 31],
+    ],
+  });
+}
 
-  console.log(2);
-  console.log(2);
+function generatePegGrammar(ruleSet: RuleSet): string {
+  return chain(ruleSet.entries())
+    .then(
+      map(([ruleNumber, rule]) => {
+        const ruleString =
+          rule.ruleType === "CharacterRule"
+            ? `"${rule.character}"`
+            : rule.possibleSequences
+                .map((sequence) => sequence.map((r) => `rule${r}`).join(" "))
+                .join(" / ");
+        return `rule${ruleNumber} = ${ruleString}`;
+      })
+    )
+    .then(toArray)
+    .then((arr) => ["start = rule0", ...arr])
+    .then((arr) => arr.join("\n"))
+    .end();
+}
+
+function numMessagesValidWithRuleSet(
+  ruleSet: RuleSet,
+  messages: string[]
+): number {
+  const parser = peg.generate(generatePegGrammar(ruleSet));
+  const messageIsValid = (message: string) => {
+    try {
+      parser.parse(message);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  };
+  return chain(messages)
+    .then(
+      map((message) => {
+        if (messageIsValid(message)) {
+          console.log(`VALID   ${message}`);
+        } else {
+          console.log(`INVALID ${message}`);
+        }
+        return message;
+      })
+    )
+    .then(countMatching(messageIsValid))
+    .end();
+}
+
+(async () => {
+  const { ruleSet, messages } = await parseInput();
+  // const part1Answer = numMessagesValidWithRuleSet(ruleSet, messages);
+  updateRuleSet(ruleSet);
+  const part2Answer = numMessagesValidWithRuleSet(ruleSet, messages);
+
+  // console.log(part1Answer);
+  console.log(part2Answer);
 })();
