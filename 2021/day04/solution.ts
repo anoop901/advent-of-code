1343915cf59fd08a993401d6e3a6c38df2ad22dd
Anoop Naravaram <anoop901@gmail.com>
2021-12-05 20:50:00 -0800

    2021 day 4 part 1

---
+++ 2021/day04/solution.ts
@@ -0,0 +1,91 @@
+import { chain } from "@anoop901/js-util";
+import {
+  allMatch,
+  anyMatch,
+  filter,
+  generate,
+  map,
+  sum,
+} from "@anoop901/js-util/iterables";
+
+type BingoBoard = BingoSquare[][];
+
+interface BingoSquare {
+  number: number;
+  marked: boolean;
+}
+
+function parseInput(input: string): {
+  calledNumbers: number[];
+  bingoBoards: BingoBoard[];
+} {
+  const [calledNumbersStr, ...bingoBoardsStrs] = input.trim().split("\n\n");
+  const calledNumbers = calledNumbersStr.split(",").map(Number);
+  const bingoBoards = bingoBoardsStrs.map((bingoBoardStr) =>
+    bingoBoardStr.split("\n").map((bingoLineStr) =>
+      bingoLineStr
+        .trim()
+        .split(/\s+/)
+        .map(Number)
+        .map((number) => ({ number, marked: false }))
+    )
+  );
+  return { calledNumbers, bingoBoards };
+}
+
+export function solution(input: string): {
+  part1Answer: number;
+  part2Answer: number;
+} {
+  const { calledNumbers, bingoBoards } = parseInput(input);
+
+  const part1Answer = playBingo(calledNumbers, bingoBoards);
+
+  return { part1Answer, part2Answer: 0 };
+}
+
+function playBingo(calledNumbers: number[], bingoBoards: BingoBoard[]): number {
+  for (const calledNumber of calledNumbers) {
+    for (const bingoBoard of bingoBoards) {
+      updateBingoBoard(bingoBoard, calledNumber);
+      if (isWinningBingoBoard(bingoBoard)) {
+        return scoreBingoBoard(bingoBoard, calledNumber);
+      }
+    }
+  }
+  return 0;
+}
+
+function scoreBingoBoard(
+  bingoBoard: BingoBoard,
+  lastCalledNumber: number
+): number {
+  const unmarkedSum = chain(bingoBoard)
+    .then(map(filter((bingoSquare) => !bingoSquare.marked)))
+    .then(map(map((bingoSquare) => bingoSquare.number)))
+    .then(map(sum))
+    .then(sum)
+    .end();
+  return unmarkedSum * lastCalledNumber;
+}
+
+function updateBingoBoard(bingoBoard: BingoBoard, calledNumber: number): void {
+  for (const bingoBoardRow of bingoBoard) {
+    for (const bingoSquare of bingoBoardRow) {
+      if (bingoSquare.number === calledNumber) {
+        bingoSquare.marked = true;
+      }
+    }
+  }
+}
+
+function isWinningBingoBoard(bingoBoard: BingoBoard): boolean {
+  const rows = bingoBoard;
+  const columns = generate(
+    (columnIndex) => rows.map((row) => row[columnIndex]),
+    rows[0].length
+  );
+  return chain([...rows, ...columns])
+    .then(anyMatch(allMatch((x) => x.marked)))
+    .end();
+}
