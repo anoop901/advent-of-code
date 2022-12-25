693e2a09e670e7229d5722057165b8b2f94eef9e
Anoop Naravaram <anoop901@gmail.com>
2022-12-24 21:21:16 -0600

    2022 day 3 part 2

--- 2022/adventofcode2022go/day03/day03.go
+++ 2022/adventofcode2022go/day03/day03.go
@@ -8,6 +8,7 @@ import (
 type rucksack struct {
 	compartment1Items []rune
 	compartment2Items []rune
+	items             []rune
 }
 
 type Solution struct {
@@ -35,7 +36,11 @@ func (s *Solution) Init(input string) error {
 		}
 		compartment1Items := items[0 : numItems/2]
 		compartment2Items := items[numItems/2 : numItems]
-		s.rucksacks = append(s.rucksacks, rucksack{compartment1Items: compartment1Items, compartment2Items: compartment2Items})
+		s.rucksacks = append(s.rucksacks, rucksack{
+			compartment1Items: compartment1Items,
+			compartment2Items: compartment2Items,
+			items:             items,
+		})
 	}
 
 	return nil
@@ -83,6 +88,79 @@ func (s *Solution) Part1() (int, error) {
 	return prioritySum, nil
 }
 
+type set[T comparable] map[T]struct{}
+
+func sliceToSet[T comparable](slice []T) set[T] {
+	result := make(set[T])
+	for _, x := range slice {
+		result[x] = struct{}{}
+	}
+	return result
+}
+
+func intersection[T comparable](set1 set[T], set2 set[T]) map[T]struct{} {
+	result := make(set[T])
+	for x1 := range set1 {
+		for x2 := range set2 {
+			if x1 == x2 {
+				result[x1] = struct{}{}
+			}
+		}
+	}
+	return result
+}
+
+func intersectionAll[T comparable](sets []set[T]) set[T] {
+	if len(sets) == 0 {
+		return make(set[T])
+	}
+	result := sets[0]
+	for _, set := range sets {
+		result = intersection(result, set)
+	}
+	return result
+}
+
+func (set set[T]) getSomeItem() (T, error) {
+	for x := range set {
+		return x, nil
+	}
+	return *new(T), fmt.Errorf("cannot get item from empty set")
+}
+
 func (s *Solution) Part2() (int, error) {
-	return 0, nil
+	numRucksacksInGroup := 3
+	groups := make([][]rucksack, 0)
+	for i := 0; i < len(s.rucksacks); i += numRucksacksInGroup {
+		if i+numRucksacksInGroup > len(s.rucksacks) {
+			return 0, fmt.Errorf(
+				"number of rucksacks %v is not a multiple of %v",
+				len(s.rucksacks),
+				numRucksacksInGroup)
+		}
+		groups = append(groups, s.rucksacks[i:i+numRucksacksInGroup])
+	}
+
+	prioritySum := 0
+	for _, group := range groups {
+		rucksackItemsInGroup := make([][]rune, 0)
+		for _, rucksack := range group {
+			rucksackItemsInGroup = append(rucksackItemsInGroup, rucksack.items)
+		}
+		rucksackItemTypesInGroup := make([]set[rune], 0)
+		for _, oneRucksackItemsInGroup := range rucksackItemsInGroup {
+			rucksackItemTypesInGroup = append(rucksackItemTypesInGroup, sliceToSet(oneRucksackItemsInGroup))
+		}
+		commonItemsSet := intersectionAll(rucksackItemTypesInGroup)
+		commonItemType, err := commonItemsSet.getSomeItem()
+		if err != nil {
+			return 0, fmt.Errorf("no common items between rucksacks %q", rucksackItemsInGroup)
+		}
+		priority, err := priorityOfItemType(commonItemType)
+		if err != nil {
+			return 0, fmt.Errorf("failed to find priority of common item %q: %v", commonItemType, err)
+		}
+		prioritySum += priority
+	}
+	return prioritySum, nil
 }
